c3b3cfd72cea1a5627303377ff86f3d6a037cf55
thnmtrn <thien.tran6@gmail.com>
2021-03-19 21:29:47 -0400

    spotify now playing

--- pages/music.js
+++ pages/music.js
@@ -1,12 +1,15 @@
+import NowPlaying from '@/components/NowPlaying'
 import TopTracks from '@/components/TopTracks'
 
 export default function Music() {
   return (
     <>
-    <h1 className="text-4xl font-bold tracking-tight ">Music</h1>
-    <p className="mt-2 text-sm text-gray-800 dark:text-gray-300">My Current Top 10 Tracks</p>
-
+      <h1 className="text-4xl font-bold tracking-tight ">Music</h1>
+      <p className="mt-2 text-sm text-gray-800 dark:text-gray-300">
+        My Current Top 10 Tracks. Auto Updated.
+      </p>
       <TopTracks />
+      <NowPlaying/>
     </>
   )
 }
