eb720133f0a7ab9686ee0186a276b3acc4798627
thnmtrn <thien.tran6@gmail.com>
2021-03-21 00:48:16 -0400

    footer change

--- components/Footer.js
+++ components/Footer.js
@@ -21,9 +21,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://thientran.me">
-            hi@thien.me
-          </Link>
+          <Link href="https://thientran.me">hi@thientran.me</Link>
         </div>
       </div>
     </footer>
