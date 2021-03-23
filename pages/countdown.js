95a159d63ccef096194ced5cf7e53634bd937497
thnmtrn <thien.tran6@gmail.com>
2021-03-22 22:54:51 -0400

    gradient countdown

--- pages/countdown.js
+++ pages/countdown.js
@@ -44,20 +44,24 @@ function CountdownPage() {
   })
   return (
     <div className="text-center content-center w-full">
-      <div className ="justify-center flex bg-rose-400 dark:bg-lime-600 py-4">
-        <Cheese className="w-12 h-12 text-amber-900 dark:text-yellow-400" />
-        </div>
-      <div className="bg-fuchsia-400 dark:bg-blue-700 flex flex-col py-2">
-        <p className="font-semibold">days: </p> <p className="text-4xl font-extrabold">{timeLeft.days}</p>
+      <div className="justify-center flex bg-fuchsia-800 dark:bg-rose-800 py-4">
+        <Cheese className="w-12 h-12 text-amber-200 dark:text-yellow-300" />
       </div>
-      <div className="bg-rose-400 dark:bg-fuchsia-600 flex flex-col py-2">
-        <p className="font-semibold">hours: </p> <p className="text-4xl font-extrabold">{timeLeft.hours}</p>
+      <div className="bg-fuchsia-700 dark:bg-rose-700 flex flex-col py-2">
+        <p className="font-semibold">days: </p>{' '}
+        <p className="text-4xl font-extrabold">{timeLeft.days}</p>
       </div>
-      <div className="bg-blue-400 dark:bg-rose-800 flex flex-col py-2">
-        <p className="font-semibold">minutes: </p> <p className="text-4xl font-extrabold">{timeLeft.minutes}</p>
+      <div className="bg-fuchsia-600 dark:bg-rose-600 flex flex-col py-2">
+        <p className="font-semibold">hours: </p>{' '}
+        <p className="text-4xl font-extrabold">{timeLeft.hours}</p>
       </div>
-      <div className="bg-amber-400 dark:bg-amber-600 flex flex-col py-2">
-        <p className="font-semibold">seconds: </p> <p className="text-4xl font-extrabold">{timeLeft.seconds}</p>
+      <div className="bg-fuchsia-400 dark:bg-rose-400 flex flex-col py-2">
+        <p className="font-semibold">minutes: </p>{' '}
+        <p className="text-4xl font-extrabold">{timeLeft.minutes}</p>
+      </div>
+      <div className="bg-fuchsia-200 dark:bg-rose-200 flex flex-col py-2">
+        <p className="font-semibold">seconds: </p>{' '}
+        <p className="text-4xl font-extrabold">{timeLeft.seconds}</p>
       </div>
     </div>
   )
